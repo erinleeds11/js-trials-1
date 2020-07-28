6a41cbccd7281793c0a6ee81df4a389f593587db
Erin Leeds <erin@erinleeds.com>
2020-07-28 23:59:06 +0000

    objects.js completed

--- objects.js
+++ objects.js
@@ -3,11 +3,35 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
-}
+    const wordCounts = {};
+    const splitPhrase = phrase.split(' ')
 
+    for (const word of splitPhrase) {
+        if (wordCounts.has(word)){
+        wordCounts[word] += 1;
+        }   else {
+            wordCounts[word] = 1 
+        }
+    }
+    return wordCounts;
+
+}
+    
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+
+    const melonPrices {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+    }
+
+    if !(melonPrices.has(price)) {
+        return;
+
+    return melonPrices[price].sort();
+
+    }
 }
